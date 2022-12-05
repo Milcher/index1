bb757aadf828937b834bbf979291a1e7b0525199
Milcher <113593402+Milcher@users.noreply.github.com>
2022-12-05 10:07:16 +0300

    Add files via upload

---
+++ cs_go/Program.cs
@@ -0,0 +1,10 @@
+﻿        int y = 0;
+        for(double x=-100;x<100;x=x+0.1){
+            double a = 2*x*x*x - 3 ;
+            double s = x*x+1;
+            double d = Math.Floor(a/s);
+        
+            if(d>9||d<11){
+              Console.WriteLine(d);
+            }
+        }
