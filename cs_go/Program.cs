3ec4d9620d9f1f90f564bd69d8207dce4ac6e3b3
Milcher <113593402+Milcher@users.noreply.github.com>
2022-12-05 11:00:42 +0300

    Add files via upload

--- cs_go/Program.cs
+++ cs_go/Program.cs
@@ -1,4 +1,5 @@
-﻿        int y = 0;
+﻿void lol1(){
+          int y = 0;
         for(double x=-100;x<100;x=x+0.1){
             double a = 2*x*x*x - 3 ;
             double s = x*x+1;
@@ -8,3 +9,41 @@
               Console.WriteLine(d);
             }
         }
+
+}
+
+
+
+//LOL 2
+void lol2(){
+ double min=0;
+        
+        Stack<double> s = new Stack<double>();
+        
+        
+        for(double x=-100;x<100;x=x+0.1){
+            double y=x*x - 3*x +15;
+            s.Push(y);
+            if (y<min){
+                min=y;
+            }
+            
+            
+        }
+        Console.WriteLine(min);
+        
+        
+        Stack<double> arr=new Stack<double>();
+        
+        for(double x=-100;x<100;x=x+0.1){
+            double a = x*x -15;
+            double s2= Math.Log(x*x+3*x-7);
+            double d = a / s2;
+            if(d==0){
+                arr.Push(d);
+            }
+        }
+        Console.WriteLine(string.Join(',',arr));
+}
+lol1();
+lol2();
